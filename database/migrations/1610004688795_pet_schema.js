a4c1be779234be659a41c12d87b0e32bd4a9b032
mabelsueh <su.eh.mabel@gmail.com>
2021-01-07 07:35:40 +0000

    added pets schema

---
+++ database/migrations/1610004688795_pet_schema.js
@@ -0,0 +1,31 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class PetSchema extends Schema {
+  up () {
+    this.create('pets', (table) => {
+      table.increments()
+      table.string('name').notNullable()
+      table.integer('age').notNullable()
+
+      // foreign keys
+      table.integer('owner_id').unsigned().notNullable()
+      table.foreign('owner_id').references('owners_id')
+
+      table.integer('breed_id').unsigned().notNullable()
+      table.foreign('breed_id').references('breeds_id')
+
+
+
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('pets')
+  }
+}
+
+module.exports = PetSchema
